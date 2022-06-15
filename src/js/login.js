c989da3886d11afb80f16826bf2885f82230da52
Samuel Jaja <samuel.jaja21@gmail.com>
2022-06-15 06:20:15 +0100

    Modified views folder

--- src/js/login.js
+++ src/js/login.js
@@ -2,19 +2,41 @@ const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 
-let username = document.getElementById("username").value;
-let email = document.getElementById("email").value;
-let password = document.getElementById("password").value;
+function UserAuth() {
+    let username = document.getElementById("username").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
 
-let user = {
-    username: username,
-    email: email,
-    password: password,
-};
+    let user = localStorage.setItem("username", username);
+    let pass = localStorage.setItem("username", password);
+    let em = localStorage.setItem("username", email);
 
-let json = JSON.stringify(user);
-localStorage.setItem(user, json);
-console.log("User Added");
+    // user = localStorage.getItem("username", username);
+    // pass = localStorage.getItem("username", password);
+    // em = localStorage.getItem("username", email);
+
+    // let a, b, c;
+
+    // a = "Jaja";
+    // b = 123456;
+    // c = "samuel.jaja21@gmail.com";
+
+    // if (a == user && b == pass && c == em) {
+    //     alert("Login Successful");
+    // } else {
+    //     alert("Invalid details !");
+    // }
+}
+
+// let user = {
+//     username: username,
+//     email: email,
+//     password: password,
+// };
+
+// let json = JSON.stringify(user);
+// localStorage.setItem(user, json);
+// console.log("User Added");
 
 sign_up_btn.addEventListener("click", () => {
     container.classList.add("sign-up-mode");
