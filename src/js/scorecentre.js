1ff4a835ce34895d05a88ca09e6bcc6b9d5e2618
Samuel Jaja <samuel.jaja21@gmail.com>
2022-06-16 15:43:30 +0100

    Updated topplayers

--- src/js/scorecentre.js
+++ src/js/scorecentre.js
@@ -7,7 +7,7 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 const MAX_HIGH_SCORES = 3;
 
-finalScore.innerText = mostRecentScore;
+finalScore.innerText = mostRecentScore + "%";
 
 username.addEventListener("keyup", () => {
     saveScoreBtn.disabled = !username.value;
@@ -26,11 +26,10 @@ saveHighScore = (e) => {
     highScores.splice(3);
 
     localStorage.setItem("highScores", JSON.stringify(highScores));
-    // window.location.assign("/");
 };
 
 const playerScore = document.getElementById("playerScore");
 const currentplayer = JSON.parse(localStorage.getItem("allUsers"));
 playerScore.innerText = currentplayer[0].usern;
 const playerscorenotify = playerScore.innerText;
-playerScore.innerText = `Hey ${playerscorenotify}, you Scored `;
+playerScore.innerText = `Hey ${playerscorenotify}, you scored `;
